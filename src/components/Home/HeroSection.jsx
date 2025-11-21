87914a17f141357a46b408ea067f67d7602050c0
Ronak <ronak21varu@gmail.com>
2025-11-22 00:12:28 +0530

    Home page done 80%

---
+++ src/components/Home/HeroSection.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Container from "../Container";
+import heroImage from "../../assets/images/heroimage.jpg";
+import PrimaryButton from "../PrimaryButton";
+import AboutStatsSection from "../AboutUs/AboutStatsSection";
+
+const HeroSection = () => {
+  return (
+    <>
+      <section
+        className="
+          relative w-full h-screen 
+          flex items-center justify-center
+        "
+      >
+        {/* Background Image */}
+        <div className="absolute inset-0">
+          <img
+            src={heroImage}
+            alt="Hero"
+            className="w-full h-full object-cover"
+          />
+
+          {/* Dark Overlay */}
+          <div className="absolute inset-0 bg-black/60"></div>
+        </div>
+
+        {/* Content */}
+        <Container>
+          <div className="relative z-10 w-full text-center px-4">
+
+            <h5 className="text-96 leading-96 font-montserrat text-primary font-semibold mx-auto">
+              Powering the Future of Industry
+            </h5>
+
+            <p className="
+              font-quicksand font-medium 
+              text-28 leading-28 text-white 
+              max-w-6xl mx-auto 
+              mt-50
+            ">
+              We specialize in manufacturing high-performance industrial components designed
+              for precision, strength, and reliability. Partnering with global industries to
+              deliver solutions that power progress.
+            </p>
+
+            {/* Buttons */}
+            <div className="flex items-center justify-center gap-3 mt-32">
+
+            <PrimaryButton title={"Explore"} pl={64} pr={64}/>
+
+        <button className='border-2 border-primary rounded-lg px-[64px] py-12'>
+            <p className='text-primary text-center font-medium font-montserrat text-20 leading-20'>Contact Us</p>
+        </button>
+            </div>
+          </div>
+        </Container>
+
+                  {/* STATS SECTION – OVERLAP LIKE IMAGE */}
+      <div className="absolute bottom-[-20vh] z-20">
+        <AboutStatsSection />
+      </div>
+      </section>
+    </>
+  );
+};
+
+export default HeroSection;
---
+++ src/components/Home/OurCompany.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import Container from '../Container'
+import workerImg from "../../assets/images/worker_aboutus.png"
+import PrimaryButton from '../PrimaryButton'
+
+const OurCompany = () => {
+  return (
+    <>
+            <Container>
+                <section className="mt-[319px]">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-40">
+                        {/* Image Section */}
+                        <div className="w-full flex justify-center items-center">
+                            <img
+                                src={workerImg}
+                                alt="Industrial Worker"
+                                className="relative w-full rounded-lg shadow-xl"
+                            />
+                        </div>
+
+
+                        {/* Content Section */}
+                        <div>
+                            <h2 className="font-montserrat text-primary text-64 leading-64 font-semibold border-b-2 border-divider pb-16">
+                                Our Company
+                            </h2>
+
+
+                            <p className="font-poppins font-normal text-20 text-bodytext leading-20 py-16">
+                                At Flow Wave we specialize in designing and manufacturing
+                                high-quality industrial parts that power progress across global
+                                industries. With a focus on precision, innovation, and
+                                reliability, we deliver solutions that meet the toughest
+                                engineering standards. Our expert team combines years of
+                                experience with advanced technology to ensure excellence in every
+                                component we produce. We believe in building long-term
+                                partnerships through trust, performance, and consistent quality.
+                                From concept to completion, we’re dedicated to engineering the
+                                future of industrial innovation.
+                            </p>
+
+
+                            <p className="font-poppins font-normal text-20 text-bodytext leading-20 pb-32">
+                                At Flow Wave we specialize in designing and manufacturing
+                                high-quality industrial parts that power progress across global
+                                industries. With a focus on precision, innovation, and
+                                reliability, we deliver solutions that meet the toughest
+                                engineering standards. Our expert team combines years of
+                                experience with advanced technology to ensure excellence in every
+                                component we produce.
+                            </p>
+
+                            <PrimaryButton title={"Know more..."} pl={24} pr={24}/>
+                        
+                        </div>
+                    </div>
+                </section>
+            </Container>
+    </>
+  )
+}
+
+export default OurCompany
