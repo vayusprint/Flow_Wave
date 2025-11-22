bd186919f49c860400fd98bdfce440ff4d231e94
Deep Padariya <deeppadariya129@gmail.com>
2025-11-22 17:15:39 +0530

    productlist bug fix

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,22 +6,22 @@ const ProductCard = ({ alt, srcImage, imageBg, productName, productDescription }
     console.log('bg' , imageBg);
     
     return (
-        <div className='flex flex-col gap-7 p-8 bg-white rounded-[15px] border border-divider'>
-            <div className='relative rounded-lg p-2 flex justify-center items-center'>
+        <div className='flex flex-col gap-28 p-32 bg-white rounded-[15px] border border-divider'>
+            <div className='relative rounded-lg p-12 flex justify-center items-center'>
                 <div className='absolute inset-0 rounded-2xl opacity-10' style={{ backgroundColor: imageBg }} ></div>
                 <img loading="lazy" alt={alt} src={srcImage} className='relative z-10 object-contain w-full h-auto max-w-[300px] max-h-[300px]' />
             </div>
-            <div className='flex flex-col gap-6'>
-                <div className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-24'>
+                <div className='flex flex-col gap-16'>
                     <p className='font-montserrat font-medium text-32 text-primaryHeading'>{productName}</p>
                     <p className='font-montserrat font-medium text-16 text-muted_text'>{productDescription}</p>
                 </div>
 
                 <div className=' border-2 border-divider rounded-full'></div>
 
-                <div className=''>
-                    <PrimaryButton title={"Product Details"} />
-                </div>
+                
+                <PrimaryButton title={"Product Details"} />
+                
             </div>
         </div>
     )
